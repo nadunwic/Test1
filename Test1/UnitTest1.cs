24fce310f3e7d96b2562f35f1c08ba45559c6ba5
OMELA <OMELA@LAPTOP-8MSIBG8I>
2022-10-04 12:26:39 +0530

    Add project files.

---
+++ Test1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Threading;
+
+namespace Test1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int waitingTime = 1000;
+
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.XPath("//div[@class='FPdoLc lJ9FBc']//center//input[@name='btnK']");
+            By googleResult = By.XPath("//h3[text()='Stack Overflow - Where Developers Learn, Share, & Build ...']");
+
+
+            IWebDriver webdriver = new ChromeDriver();
+            webdriver.Manage().Window.Maximize();
+            Thread.Sleep(waitingTime);
+
+            webdriver.Navigate().GoToUrl("https://www.google.lk/");
+            Thread.Sleep(waitingTime);
+
+            webdriver.FindElement(googleSearchBar).SendKeys("stack overflow");
+            Thread.Sleep(waitingTime);
+
+            webdriver.FindElement(googleSearchButton).Click();
+            Thread.Sleep(waitingTime);
+
+            webdriver.FindElement(googleResult).Click();
+            Thread.Sleep(waitingTime);
+
+
+            webdriver.Quit();
+        }
+    }
+}
